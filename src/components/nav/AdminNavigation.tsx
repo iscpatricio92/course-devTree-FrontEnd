25c9e88e86652935f78f6d28fd4ae30bd87ab6bf
PAt from Sit <isc.patricio@gmail.com>
2025-03-04 19:08:01 -0600

    chore: Refactor AdminNavigation and SignUpView components

--- src/components/nav/AdminNavigation.tsx
+++ src/components/nav/AdminNavigation.tsx
@@ -1,11 +1,19 @@
+import { useQueryClient } from "@tanstack/react-query"
+
 const AdminNavigation = () => {
+  const queryClient = useQueryClient()
+  const signOut = () => {
+    localStorage.removeItem('devTree_access_token')
+    queryClient.invalidateQueries({queryKey:['profile']})
+
+  }
   return (
     <button
-                            className=" bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer"
-                            onClick={() => { }}
-                        >
-                            Cerrar Sesión
-                        </button>
+      className=" bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer"
+      onClick={signOut}
+    >
+      Cerrar Sesión
+    </button>
   )
 }
 
--- src/views/SignUpView.tsx
+++ src/views/SignUpView.tsx
@@ -2,33 +2,33 @@ import { Link, useLocation, useNavigate } from "react-router-dom"
 import { useForm } from 'react-hook-form'
 import ErrorMessage from "../components/ErrorMessage";
 import { SignUpForm } from "../types";
-import axios, {isAxiosError} from "axios";
+import { isAxiosError } from "axios";
 import { toast } from "sonner";
 import api from "../config/axios";
 
 const SignUpView = () => {
-  const location= useLocation();
-  const initialValues ={
+  const location = useLocation();
+  const initialValues = {
     name: '',
     email: '',
-    handle: location.state?.handle ?? '',
+    handle: location?.state?.handle ?? '',
     password: '',
     password_confirmation: ''
   }
-  const navigate=useNavigate();
-  const { register, watch, handleSubmit, reset, formState:{errors} } = useForm<SignUpForm>({ defaultValues: initialValues });
-  
-  
+  const navigate = useNavigate();
+  const { register, watch, handleSubmit, reset, formState: { errors } } = useForm<SignUpForm>({ defaultValues: initialValues });
+
+
   const password = watch('password');
-  const handleRegister = async(formData: SignUpForm) => {
-    try{
-      const {data} = await api.post(`/auth/sign-up`,formData)
+  const handleRegister = async (formData: SignUpForm) => {
+    try {
+      const { data } = await api.post(`/auth/sign-up`, formData)
       toast.success(data.message);
-      navigate('/auth/sign-in');
       reset()
+      navigate('/auth/sign-in');
     }
-    catch(error){
-      if(isAxiosError(error) && error.response){
+    catch (error) {
+      if (isAxiosError(error) && error.response) {
         toast.error(error.response.data.error);
         console.log(error.response.data.error);
       }
@@ -38,25 +38,25 @@ const SignUpView = () => {
 
   return (
     <>
-    <h1 className="text-4xl text-white font-bold">Sign Up</h1>
-    <form
-    onSubmit={handleSubmit(handleRegister)}
-    className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
-    >
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="name" className="text-2xl text-slate-500">Nombre</label>
-        <input
+      <h1 className="text-4xl text-white font-bold">Sign Up</h1>
+      <form
+        onSubmit={handleSubmit(handleRegister)}
+        className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+      >
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="name" className="text-2xl text-slate-500">Nombre</label>
+          <input
             id="name"
             type="text"
             placeholder="Your Name"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
             {...register('name', { required: 'Name is required' })}
-        />
-        {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
-    </div>
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
-        <input
+          />
+          {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
+        </div>
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
+          <input
             id="email"
             type="email"
             placeholder="Your Email"
@@ -68,60 +68,60 @@ const SignUpView = () => {
                 message: "E-mail is not valid",
               },
             })}
-        />
-        {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
-    </div>
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
-        <input
+          />
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+        </div>
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
+          <input
             id="handle"
             type="text"
             placeholder="Username without spaces"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
             {...register('handle', { required: 'Handle is required' })}
-        />
-        {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
-    </div>
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
-        <input
+          />
+          {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
+        </div>
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
+          <input
             id="password"
             type="password"
             placeholder="Password"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-            {...register('password', { 
+            {...register('password', {
               required: 'Password is required',
-              minLength:{value:8, message: 'Password must have at least 8 characters'}
+              minLength: { value: 8, message: 'Password must have at least 8 characters' }
 
             })}
-        />
-        {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
-    </div>
+          />
+          {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
+        </div>
 
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="password_confirmation" className="text-2xl text-slate-500">Repetir Password</label>
-        <input
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="password_confirmation" className="text-2xl text-slate-500">Repetir Password</label>
+          <input
             id="password"
             type="password"
             placeholder="Confirm Password"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-            {...register('password_confirmation', { 
+            {...register('password_confirmation', {
               required: 'Password is required',
               validate: value => value === password || 'The passwords do not match'
             })}
-        />
-        {errors.password_confirmation && <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage>}
-    </div>
+          />
+          {errors.password_confirmation && <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage>}
+        </div>
 
-    <input
-        type="submit"
-        className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
-        value='Create Account'
-    />  
-</form>
-    <nav className="mt-10">
-      <Link className="text-center text-white text-lg block" to="/auth/sign-in">Sign In</Link>
-    </nav>
+        <input
+          type="submit"
+          className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+          value='Create Account'
+        />
+      </form>
+      <nav className="mt-10">
+        <Link className="text-center text-white text-lg block" to="/auth/sign-in">Sign In</Link>
+      </nav>
     </>
   )
 }
